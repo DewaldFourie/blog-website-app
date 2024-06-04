2ebb2cbbe7a01e25d5d302d2db36560c2b07b971
Dewald <dewaldfourie0808@gmail.com>
2024-06-04 19:00:43 +0200

    updated base to add navigate function on load

--- src/base.jsx
+++ src/base.jsx
@@ -1,11 +1,20 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+import { useEffect } from "react";
 
 // The BASE component of the web app the loads the navbar and footer and 
 // uses an Outlet to render the different client side routes
 
 const Base = () => {
+
+    // this method will render the home page on load
+    const navigate = useNavigate();
+    useEffect(() => {
+        // Programmatically navigate to the /home path when the component mounts
+        navigate('/home')
+    }, [navigate]);   // dependency array ensures this method runs only once
+
 
     return (
         <>  

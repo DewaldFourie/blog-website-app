d043cddae6af907b8f85e4c4f9b65a84acc9b50a
Dewald <dewaldfourie0808@gmail.com>
2024-06-10 12:03:57 +0200

    updated home and about page layout and styles

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,7 +6,30 @@ const About = () => {
     return (
         <>
             <div className="about-content">
-                <h1>About</h1>
+                <div className="about-container">
+                    <div className="about-info-container">
+                        <h1>About the Project</h1>
+                        <h4>How does it work</h4>
+                        <span>This website is part of an assignment in The Odin Project, aiming to create a full-stack Blog. This project is a great way to practice and see the benefits of creating an API only-backend. The important exercise here was to set up the API and access it from the outside to demonstrate the power and flexibility of separating the back-end code from the front-end code. To achieve this, I had to create one back-end API and two front-ends websites. As said in The Odin Project: it is set up that way, because we can. Disclaimer: All posts are lorem ipsum.</span>
+                    </div>
+                    <div className='about-project-layout-header'>
+                        <span>One Project. Three Parts.</span>
+                    </div>
+                    <div className="about-project-layout-container">
+                        <div className="project-layout-item">
+                            <span>Website</span>
+                        </div>
+                        <div className="project-layout-item">
+                            <span>API</span>
+                        </div>
+                        <div className="project-layout-item">
+                            <span>CMS</span>
+                        </div>
+                    </div>
+                    <div className='about-project-info-container'>
+                        <span className='about-project-info-text'>Find out more about this specific project on this page.</span>
+                    </div>
+                </div>
             </div>
         </>
     )
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,10 +2,19 @@ import './styles/home.css'
 
 const Home = () => {
 
+    
+
     return (
         <>
             <div className="home-content">
-                <h1>HOME</h1>
+                <div className="banner-container">
+                    <div className="banner-container-info">
+                        <h1 className="banner-container-text">HOME Banner text</h1>
+                    </div>
+                </div>
+                <div className="segment-1-container">
+                    <h2>This is where the post will be</h2>
+                </div>
             </div>        
         </>
     )

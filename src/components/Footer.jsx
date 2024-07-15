7e8fb91586b74e66e8ee66648a19fd7e125ed6e1
Dewald <dewaldfourie0808@gmail.com>
2024-07-15 08:06:38 +0200

    updated footer links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -13,7 +13,7 @@ const Footer = () => {
                     <a className='t-c-s-link' href="#">Terms & Conditions</a>
                 </div>
                 <div className='returns-container'>
-                    <a className='return-policy-link' href="#">Github</a>
+                    <a className='return-policy-link' href="https://github.com/DewaldFourie/blog-website-app" target='_blank' rel='noopener noreferrer'>Github</a>
                 </div>
                 <div className='footer-copyright-container'>
                     <span className='copyright-text'>©TravelBLOG created by DewaldFourie 2024</span>
@@ -22,9 +22,15 @@ const Footer = () => {
                     <a className='help-centre-text' href="#">Help Center</a>
                 </div>
                 <div className='footer-socials-container'>
-                    <img className='social-icon' src={whatsappIcon} alt="whatsapp" />
-                    <img className='social-icon' src={facebookIcon} alt="facebook" />
-                    <img className='social-icon' src={instagramIcon} alt="instagram" />
+                    <a href="https://web.whatsapp.com/" target="_blank" rel="noopener noreferrer">
+                        <img className='social-icon' src={whatsappIcon} alt="whatsapp" />
+                    </a>
+                    <a href="https://www.facebook.com/theodinproject/" target="_blank" rel="noopener noreferrer">
+                        <img className='social-icon' src={facebookIcon} alt="facebook" />
+                    </a>
+                    <a href="https://www.instagram.com/theodinproject/" target="_blank" rel="noopener noreferrer">
+                        <img className='social-icon' src={instagramIcon} alt="instagram" />
+                    </a>
                 </div>
             </div>
         </>

06c989d9dec23a4b11462fa8fea6eb967c3aa546
Dewald <dewaldfourie0808@gmail.com>
2025-05-08 08:17:18 +0200

    updated about section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -10,7 +10,7 @@ const About = () => {
                     <div className="about-info-container">
                         <h1 className='about-info-heading'>About the Project</h1>
                         <h4 className='about-info-subheading'>How does it work?</h4>
-                        <span className='about-info-text'>This website is part of an assignment in The Odin Project, aiming to create a full-stack Blog. This project is a great way to practice and see the benefits of creating an API only-backend. The important exercise here was to set up the API and access it from the outside to demonstrate the power and flexibility of separating the back-end code from the front-end code. To achieve this, I had to create one back-end API and two front-ends websites. As said in The Odin Project: it is set up that way, because we can. Disclaimer: All posts are AI generated.</span>
+                        <span className='about-info-text'>This website was built as part of an Odin Project assignment focused on creating a full-stack blog with an API-only backend. The goal was to demonstrate the flexibility of separating backend and frontend logic. To achieve this, I built one backend API and two independent frontend sites. Disclaimer: All posts are AI-generated.</span>
                     </div>
                     <div className='about-project-layout-header'>
                         <span>One Project. Three Parts.</span>
